b86ae2064e61c935bd54bf70b6d8739e86f21b12
jm <jm@disuan.co>
2019-07-21 14:09:00 +0400

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
-#! /bin/bash
-rm -rf dist && NODE_ENV=production npx babel src/lib --out-dir dist --copy-files
+#!/bin/bash
+rm -rf dist && NODE_ENV=production  npx babel src/lib --out-dir dist --copy-files --ignore __tests__,spec.js,test.js,__snapshots__
